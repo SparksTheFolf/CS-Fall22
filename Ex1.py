5fa7896435236406440f04a7a797547f758406f3
Nolan <officialstackemup@gmail.com>
2022-09-05 15:12:16 -0700

    update

---
+++ Ex1.py
@@ -0,0 +1,7 @@
+print("hebjherg")
+print("ew fwe ")
+print("")
+print("")
+print("")
+print("")
+print("")
---
+++ ex3.py
@@ -0,0 +1,15 @@
+print("I will now count my chickens")
+#BRUH
+print("Hens", + 25 +30 / 6)
+print("Roosters", 100 - 25 * 3)
+
+print("Now I count the egss: ")
+
+print(3 + 2 + 1 -5 +4 % 2 - 1 /4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3+2<5-7)
+
+print("What is 3 + 2", 3+2)
+
