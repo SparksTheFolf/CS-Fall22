435efbd67709ea0a3d3ce09a94c701445338d32d
Nolan <officialstackemup@gmail.com>
2022-09-12 19:09:04 -0700

    BRUHHHHH GAHE!!!

---
+++ inClassKeyManagement.py
@@ -0,0 +1,18 @@
+import os
+import math
+from numpy import random
+from pandocfilters import Str
+
+# !
+
+Rand_Key = random.randint(10)
+
+main_key = Str(Rand_Key)
+
+for x in Rand_Key:
+  print(x)
+
+Correct_Key = random.randint(10)
+
+# ! refractor /usr/nooln/
+# open the mailbox
