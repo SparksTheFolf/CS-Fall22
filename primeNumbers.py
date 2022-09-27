b79c34cac711e2c609e564f3e8c662c587d9c50b
Nolan <officialstackemup@gmail.com>
2022-09-27 15:55:44 -0700

    update

--- primeNumbers.py
+++ primeNumbers.py
@@ -6,21 +6,21 @@ Online Version Avail: https://s.wuffs.net/CS-Fall22
 This program is designed to take the input of a prime number {prime} and determine if it is a Prime number {TRUE} or
 not a prime number {FALSE}, then print the max values {max_value}.
 """
-prm_number = []
-number = []
-repeat = 5
+
+prm_number = []  # Adds the primed number to list
+number = []  # Adds the main input number to list
+repeat = 5  # Repeats the program 5 times
 
 for x in range(repeat):
     print()  # Empty Line For Readability
     prime = int(input("Enter a possible prime number greater than 1: "))
     number.append(prime)
-    #    def main():
     if prime <= 1:
         print(prime, f"is not a prime number because it is less than 1")
     if prime > 1:
         for num in range(2, prime):
-            if (prime % num) == 0:
-                print(prime, f"is not a prime number because it is devisable by {num}")
+            if (prime % num) == 0:  # Prime Number Formula
+                print(f"{prime} is not a prime number because it is devisable by {num}")
                 break
         else:
             print(prime, f"is a prime number")
---
+++ range.py
@@ -0,0 +1,2 @@
+for i in range(0,10000, 10):
+    print(i)
