9c7dcbba3fd841122274305b79ced51bd289e01e
Nolan <officialstackemup@gmail.com>
2022-09-29 14:29:45 -0700

    Update primeNumbers.py

--- primeNumbers.py
+++ primeNumbers.py
@@ -9,35 +9,41 @@ not a prime number {FALSE}, then print the max values {max_value}.
 
 prm_number = []  # Adds the primed number to list
 number = []  # Adds the main input number to list
-max_num = prm_number
 repeat = 5  # Repeats the program 5 times
-start_max_num = 0
+start_max_num = 0  # For max
 
 
-for x in range(repeat):
-    print()  # Empty Line For Readability
-    prime = int(input("Enter a possible prime number greater than 1: "))
-    number.append(prime)
-    if prime <= 1:
-        print(f"{prime} is not a prime number because it is less than 1")
-    if prime > 1:
-        for num in range(2, prime):
-            if (prime % num) == 0:  # Prime Number Formula
-                print(f"{prime} is not a prime number because it is devisable by {num}")
-                break
-        else:
-            print(f"{prime} is a prime number")
-            prm_number.append(prime)
+def main():
+    global start_max_num
+    for x in range(repeat):
+        print()  # Empty Line For Readability
+        prime = int(input("Enter a possible prime number greater than 1: "))
+        number.append(prime)
+        if prime <= 1:
+            print(f"{prime} is not a prime number because it is less than 1, Restarting the Program")
+            main()
+        if prime > 1.1:
+            for num in range(2, prime):
+                if (prime % num) == 0:  # Prime Number Formula
+                    print(f"{prime} is not a prime number because it is devisable by {num}")
+                    break
+            else:
+                print(f"{prime} is a prime number")
+                prm_number.append(prime)
 
 #  Bonus Points Section
-if repeat >= 5:
-    length_num = len(number)
-    length_prm = len(prm_number)
+    if repeat >= 5:
+        length_num = len(number)
+        length_prm = len(prm_number)
 
-    for number in prm_number:
-        if number > start_max_num:
-            start_max_num = number
+        for looking_num in prm_number:
+            if looking_num > start_max_num:
+                start_max_num = looking_num
 
-    print()  # Empty Line For Readability
-    print(f"{length_num} of numbers were examined, {length_prm} of those were prime,"
-          f" and the largest was {start_max_num}.")
+        print()  # Empty Line For Readability
+        print(f"{length_num} of numbers were examined, {length_prm} of those were prime,"
+              f" and the largest was {start_max_num}.")
+
+
+main()  # Calls the Main method
+
