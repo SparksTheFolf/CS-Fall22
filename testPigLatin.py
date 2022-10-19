4b8b443bb7290cacf2b5d6f320b8988df26f65fa
Nolan <officialstackemup@gmail.com>
2022-10-19 11:06:15 -0700

    Update testPigLatin.py

--- testPigLatin.py
+++ testPigLatin.py
@@ -4,16 +4,19 @@ Author: Nolan T. (CS118 Fall Class)
 Last Edited: 10/18/2022 Online Version Avail: https://github.com/SparksTheFolf/CS-Fall22
 Purpose: This program is designed to take the input of a sentence, and convert it to pig latin, and then convert it back to regular plain english, and detect it.
 """
-theInput = input('\n\nInput a sentence to be translated to pig-latin:\n\n').lower()
-translationToPL = theInput.split()
+while True:
+    theInput = input('\n\nInput a sentence to be translated to pig-latin:\n\n').lower()
+    translationToPL = theInput.split()
+    if not theInput:
+        print("No string inputted, ending program.")
+        break
+    for secondary, words in enumerate(translationToPL):
+        if words[0] in 'aeiou':
+            translationToPL[secondary] = translationToPL[secondary] + "-yay"
+        else:
+            for third, char in enumerate(words):
+                if char in 'aeiou':
+                    translationToPL[secondary] = words[third:] + "-" + words[:third] + "ay"
+                    break
 
-for secondary, words in enumerate(translationToPL):
-    if words[0] in 'aeiou':
-        translationToPL[secondary] = translationToPL[secondary] + "-yay"
-    else:
-        for third, char in enumerate(words):
-            if char in 'aeiou':
-                translationToPL[secondary] = words[third:] + "-" + words[:third] + "ay"
-                break
-
-print('Translated to Pig-Latin: ' + ' '.join(translationToPL))
+    print('Translated to Pig-Latin: ' + ' '.join(translationToPL))
