c1e5b47c6c0f44ae7c1f5a09aa2fa98ed5af994d
Nolan <officialstackemup@gmail.com>
2022-10-19 11:42:45 -0700

    Update testPigLatin.py

--- testPigLatin.py
+++ testPigLatin.py
@@ -1,12 +1,12 @@
 """
 English to Pig-Latin, Pig-Latin to english translator !!!!!!!!TESTING!!!!!!!!!!!!!!!!!!!
 Author: Nolan T. (CS118 Fall Class)
-Last Edited: 10/18/2022 Online Version Avail: https://github.com/SparksTheFolf/CS-Fall22
+Last Edited: 10/19/2022 Online Version Avail: https://github.com/SparksTheFolf/CS-Fall22
 Purpose: This program is designed to take the input of a sentence, and convert it to pig latin, and then convert it back to regular plain english, and detect it.
 """
 while True:
     theInput = str(input('\n\nInput a sentence to be translated:\n')).lower()
-    remove = theInput.replace('?' or '.', '')
+    remove = theInput.replace('?', '')
     trans = remove.split()
     if not theInput:
         print("No string inputted, ending program.")
