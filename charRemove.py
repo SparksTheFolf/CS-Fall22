f0f625e1de5446f5819bc90f93792a25d47a8dac
Nolan <officialstackemup@gmail.com>
2022-10-19 12:07:26 -0700

    replacement function

--- charRemove.py
+++ charRemove.py
@@ -1,2 +1,4 @@
-theInput = str(input('\n\nInput a sentence to be translated:\n')).lower()
-print(theInput.replace('?', ''))
+while True:
+    theInput = str(input('\n\nInput a sentence to be translated:\n')).lower()
+    remove = theInput.replace('?', '').replace('.', '').replace(',', '')
+    print(remove)
--- testPigLatin.py
+++ testPigLatin.py
@@ -6,7 +6,7 @@ Purpose: This program is designed to take the input of a sentence, and convert i
 """
 while True:
     theInput = str(input('\n\nInput a sentence to be translated:\n')).lower()
-    remove = theInput.replace('?', '')
+    remove = theInput.replace('?', '').replace('.', '').replace(',', '')
     trans = remove.split()
     if not theInput:
         print("No string inputted, ending program.")
