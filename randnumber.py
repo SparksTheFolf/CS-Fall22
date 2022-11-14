4ebeec75b07572d6c304d71362b1f361f5ed43ea
Nolan <officialstackemup@gmail.com>
2022-11-14 10:01:58 -0700

    Update randnumber.py

--- randnumber.py
+++ randnumber.py
@@ -1,16 +1,29 @@
 from random import randint
+from typing import Type
 
 print("Creating all the integers from 1 to n (inclusive) randomly.")
+s = int(input("Please enter the upper boundary, n = "))
+n = s
+lst = []
+num = {}
 
-n = input("Please enter the upper boundary, n = ")
 
-
-def new_random():
-    if type(n) == int:
+def new_random() -> Type[int]:
+    if type() == int:
         p = int(n)
-        print("ok")
+        p = randint(1, p)
+        p.append(lst)
+        return int
     elif type(n) == str:
         print("not ok")
 
 
-new_random()
+def back_to_back() -> (int, int):
+    """ returns a tuple
+         1st item is the integer that was generated back to back most often
+         2nd item is the mac length of the series
+    """
+
+print(f"Creating a random sequence of all integers 1 .. {n} required {k} randint(1,{n}) calls")
+print(f"The integer that was created last was {last}.")
+print(f"The integer that was created the most times in a row, {how_often} times, was {most_often}.")
