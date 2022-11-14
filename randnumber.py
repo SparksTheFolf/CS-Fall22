dbf9baa2bf51b8ed21b1bbc24d0119bb2392f47f
Nolan <officialstackemup@gmail.com>
2022-11-14 13:43:45 -0700

    Update randnumber.py

--- randnumber.py
+++ randnumber.py
@@ -2,7 +2,9 @@
 Author: Nolan T. (CS118 Fall Class)
 Last Edited: 11/14/2022
 Online Version Avail: https://github.com/SparksTheFolf/CS-Fall22
-This program will generate a random number between 1 and n and then repeat until n number of times is generated. Then it will print the number of times the random number generator was called, the last number generated, the number of times the most common integer was generated, and the most common integer.
+This program will generate a random number between 1 and n and then repeat until n number of times is generated.
+Then it will print the number of times the random number generator was called, the last number generated, the number of times
+the most common integer was generated, and the most common integer.
 """
 
 from random import randint
@@ -11,7 +13,6 @@ print("Creating all the integers from 1 to n (inclusive) randomly.")
 n = input("Please enter the upper boundary, n = ")
 randomlist = []
 
-"""Write a Python program that generates random numbers 1 to n (inclusive), until all integers 1 to n have been generated at least once."""
 
 if not n.isdigit():
     print("Please enter a valid number.")
